4c98f7c41af9a6c415479c3c2adc481ccac9d22c
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-29 20:30:27 +0530

    Update to class

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component{
+  render(){
+      return(
+        <div className="App">
+        <Todos />
+        </div>
+      )
+  }
 }
+
 
 export default App;

d45a9a6add11dd6fa8e844855699697374168eb0
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-29 20:44:39 +0530

    Update Todo js

--- myapp/src/components/Todos.js
+++ myapp/src/components/Todos.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Todoitems from './Todoitems';
 
 class Todos extends Component{
     render(){
         //console.log(this.props.todos)
         return this.props.todos.map((todo) =>(
-            <h3>{ todo.title }</h3>
+            <Todoitems />
         ));
     }
 }

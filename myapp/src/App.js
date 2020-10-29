9b130e86acdcb69194146865bef7edf678ea4cfe
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-29 20:27:51 +0530

    Update app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <Todos />
     </div>
   );
 }

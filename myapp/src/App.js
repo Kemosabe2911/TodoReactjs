2cec780ad82e89dc8236bea1a029b77ec554501a
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-29 20:34:07 +0530

    Add states

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component{
+  state= {
+    todo:[
+      {
+        id: 1,
+        title: 'Do homework',
+        completed: false 
+      },
+      {
+        id: 2,
+        title: 'Do homework2',
+        completed: false 
+      },
+      {
+        id: 2,
+        title: 'Do homework3',
+        completed: false 
+      }
+    ]
+  }
+
+
   render(){
       return(
         <div className="App">

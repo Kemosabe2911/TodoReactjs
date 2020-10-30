0d28a3fc12ffb985458553fb959f9c78547514fa
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-30 12:21:39 +0530

    Update adddtodo

--- myapp/src/components/AddTodo.js
+++ myapp/src/components/AddTodo.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export default class AddTodo extends Component {
+    state= {
+        title: ''
+    }
+
+    onChange =(e) =>{
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
         return (
             <form style={{display: "flex"}} className="form">
-                <input style={{flex:'10', padding: "5px"}} type="text" name="title" placeholder="Add Todo" />
+                <input style={{flex:'10', padding: "5px"}} 
+                    type="text" 
+                    name="title" 
+                    placeholder="Add Todo" 
+                    value={this.state.title} 
+                    onChange={this.onChange} />
                 <input style={{flex:'1'}} type="submit" value="Submit" className="btn" />
             </form>
         )

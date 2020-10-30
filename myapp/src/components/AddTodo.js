0ef4b01f12bc28f6199ea25e0f974543b5e36aae
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-30 12:16:00 +0530

    Update css

--- myapp/src/components/AddTodo.js
+++ myapp/src/components/AddTodo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class AddTodo extends Component {
     render() {
         return (
-            <form style={{display: "flex"}}>
+            <form style={{display: "flex"}} className="form">
                 <input style={{flex:'10', padding: "5px"}} type="text" name="title" placeholder="Add Todo" />
                 <input style={{flex:'1'}} type="submit" value="Submit" className="btn" />
             </form>

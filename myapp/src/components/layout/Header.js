e64505365f1c8b1ccd893568752101687ac78137
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-30 12:59:00 +0530

    Add links in headet

--- myapp/src/components/layout/Header.js
+++ myapp/src/components/layout/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header style={headerStyle}>
             <h1>Todo List</h1>
+            <Link style={LinkStyle} to="/">Home</Link>{' '} | {' '} <Link style={LinkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -13,4 +15,8 @@ const headerStyle ={
     color: "#fff",
     textAlign: "center",
     padding: "10px"
+}
+
+const LinkStyle ={
+    color: "#ffffff"
 }

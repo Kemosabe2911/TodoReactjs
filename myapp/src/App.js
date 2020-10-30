29101041c2c5c56d30bcfefc6b391d2443753353
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-30 11:37:10 +0530

    Set state

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -24,7 +24,13 @@ class App extends Component{
   }
 
   markComplete = (id) =>{
-    console.log(id);
+    //console.log(id);
+    this.setState( { todos: this.state.todos.map( todo => {
+      if(todo.id === id){
+        todo.completed= !todo.completed;
+      }
+      return todo;
+    })});
   }
 
   render(){

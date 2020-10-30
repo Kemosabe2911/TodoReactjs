ce31bf51e9e06683694b4facece38de3dcc4555f
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-30 12:08:29 +0530

    Add Addtodo

---
+++ myapp/src/AddTodo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AddTodo extends Component {
+    render() {
+        return (
+            <form style={{display: "flex"}}>
+                <input style={{flex:'10', padding: "5px"}} type="text" name="title" placeholder="Add Todo" />
+                <input style={{flex:'1'}} type="submit" value="Submit" className="btn" />
+            </form>
+        )
+    }
+}

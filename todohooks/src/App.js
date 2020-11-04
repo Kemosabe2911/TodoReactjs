75107a83e0e6712f66429b4b4692669f63e5f6a8
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-04 14:01:55 +0530

    Update app.css

--- todohooks/src/App.js
+++ todohooks/src/App.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
+function Todo({todo, index}){
+  return(
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
 function App() {
   const [todos, setTodos]= useState([
     {
@@ -19,7 +28,11 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="container">
+        {todos.map((todo, index) =>(
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }

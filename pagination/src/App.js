e6c10a9153e68e0e70f43810ea8f5f5db785f2bb
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-04 18:22:17 +0530

    Add json placeholder

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -3,6 +3,24 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [posts, setPosts]= useState([]);
+  const [loading, setLoading]= useState(false);
+  const [currentPage, setCurrentPage]= useState(1);
+  const [postsPerPage, setPostsPerPage]= useState(10);
+
+  useEffect(() =>{
+    const fetchPosts= async () =>{
+      setLoading(true);
+      const res= await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="container">
       

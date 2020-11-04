ccda6fa892807d408be273a9c2d5387e7ed8a012
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-04 18:27:32 +0530

    Add posts.js

---
+++ pagination/src/components/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Posts = ({ posts, loading }) => {
+    if(loading){
+        return <h2>Loading...</h2>
+    }
+    return (
+        <ul className="list-group mb-4">
+            {posts.map((post => (
+              <li key={post.id} className="list-group-item">
+                  {post.title}
+              </li>  
+            )))}
+        </ul>
+    )
+}

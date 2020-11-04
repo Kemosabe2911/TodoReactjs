2a9793e3052531065cad0edbb30eb31c7da9fd5d
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-04 13:54:54 +0530

    Add const todos

--- todohooks/src/App.js
+++ todohooks/src/App.js
@@ -1,7 +1,22 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos]= useState([
+    {
+      text: 'Todo List item1',
+      isCompleted: false
+    },
+    {
+      text: 'Todo List item2',
+      isCompleted: false
+    },
+    {
+      text: 'Todo List item3',
+      isCompleted: false
+    }
+  ]);
+
   return (
     <div className="App">
       
